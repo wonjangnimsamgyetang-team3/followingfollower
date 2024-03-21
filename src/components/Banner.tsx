087c2352829d89151dda1c4014ec94178fdd6e96
kimsohyeon <aotoyae@gmail.com>
2024-03-21 20:42:57 +0900

    [feat/#47] - 메인페이지 배너 추가

---
+++ src/components/Banner.tsx
@@ -0,0 +1,10 @@
+const Banner = () => {
+  return (
+    <article className="pt-24 h-60 ">
+      <h1 className="pl-8 text-4xl">오늘 할 일, 🗓️</h1>
+      <h1 className="pt-3 pl-8 text-4xl">팔팔에서 쉽게 기록하고 공유해봐요!</h1>
+    </article>
+  );
+};
+
+export default Banner;
