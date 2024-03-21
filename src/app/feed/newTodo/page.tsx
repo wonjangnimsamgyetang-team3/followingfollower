27a17b120c7ffdedac18535510a232d71cfd7cff
kewii33 <kewiii071@gmail.com>
2024-03-21 16:25:34 +0900

    commit to merge

--- src/app/feed/newTodo/page.tsx
+++ src/app/feed/newTodo/page.tsx
@@ -1,6 +1,27 @@
-import NewTodo from '../../../components/NewTodo';
+'use client';
 
-const NewPostPage = async () => {
+import NewTodo from '../../../components/NewTodo';
+import { queryKey } from '@/query/queryKey';
+import { readUserInfo } from '@/supabase/myPage/profileImage';
+import { supabase } from '@/supabase/supabase';
+import { useQuery } from '@tanstack/react-query';
+
+const NewPostPage = () => {
+  const {
+    isPending,
+    isError,
+    data: userInfo,
+  } = useQuery({
+    queryKey: [queryKey.usersAccounts],
+    queryFn: readUserInfo,
+  });
+
+  const currentUser = supabase.auth.getUser();
+
+  console.log(userInfo);
+  console.log('현재 로그인한 사용자 정보:', currentUser);
+
+  console.log(userInfo);
   return (
     <div>
       <NewTodo />
