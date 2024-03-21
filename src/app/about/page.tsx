a72cd0c7d812612334f48268bae883faaac8976b
kimsohyeon <aotoyae@gmail.com>
2024-03-21 14:48:46 +0900

    [style] - 전체적으로 정렬 수정

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,53 +4,83 @@ import boy from '../../assets/boy.png';
 import memo from '../../assets/memo.png';
 import calendar from '../../assets/calendar.png';
 import comment from '../../assets/comment.png';
+import { supabase } from '@/supabase/supabase';
 
-const AboutPage = () => {
+export const revalidate = 10;
+
+const AboutPage = async () => {
+  const { data: todos } = await supabase.from('TodoList').select('*');
+  const { data: users } = await supabase.from('usersAccounts').select('*');
+
+  const todosNum = todos?.length;
+  console.log(todos?.length);
+  const usersNum = users?.length;
+  console.log(users?.length);
+
   return (
-    <div>
-      <h2>Todo를 공유하며 소통할 수 있는 사이트 팔팔입니다!</h2>
-      <article>
+    <main className="p-7 flex flex-col place-items-center">
+      <h2 className="p-3 text-4xl text-subColor1 border-b-2 border-solid border-subColor1">
+        Todo를 공유하며 소통할 수 있는 사이트 팔팔입니다!
+      </h2>
+      <article className="pt-14 flex place-items-center gap-14">
         <section>
-          <Image src={girl} alt="소개하는 여자 일러스트" />
+          <Image
+            src={girl}
+            width={240}
+            height={240}
+            alt="소개하는 여자 일러스트"
+          />
         </section>
-        <section>
-          <div>
+        <section className="flex flex-col gap-7">
+          <div className="flex place-items-center justify-between gap-20">
             <p>“자신의 Todo를 공유해보세요!”</p>
-            <Image src={memo} alt="메모지 일러스트" />
+            <Image src={memo} width={100} height={100} alt="메모지 일러스트" />
           </div>
-          <div>
+          <div className="flex place-items-center justify-between">
             <p>“캘린더로 월별 일정을 보기 편하게!”</p>
-            <Image src={calendar} alt="캘린더 일러스트" />
+            <Image
+              src={calendar}
+              width={100}
+              height={100}
+              alt="캘린더 일러스트"
+            />
           </div>
-          <div>
+          <div className="flex place-items-center justify-between">
             <p>“서로의 꿈과 목표를 응원해요!”</p>
-            <Image src={comment} alt="댓글 일러스트" />
+            <Image src={comment} width={100} height={100} alt="댓글 일러스트" />
           </div>
         </section>
       </article>
-      <article>
-        <section>
-          <div>
-            <h1>103</h1>
+      <article className="pt-14 flex place-items-center gap-14">
+        <section className="flex flex-col place-items-end gap-7">
+          <div className="flex place-items-center justify-center gap-2">
+            <h1 className="text-8xl text-subColor1">{todosNum}</h1>
             <p>개의 Todo가 공유되었어요!</p>
           </div>
-          <div>
-            <h1>57</h1>
+          <div className="flex place-items-center justify-center gap-2">
+            <h1 className="text-8xl text-subColor1">57</h1>
             <p>개의 Todo가 완료되었어요!</p>
           </div>
-          <div>
-            <h1>34</h1>
+          <div className="flex place-items-center justify-center gap-2">
+            <h1 className="text-8xl text-subColor1">{usersNum}</h1>
             <p>명의 사람들이 이용 중이에요!</p>
           </div>
         </section>
         <section>
-          <Image src={boy} alt="소개하는 남자 일러스트" />
+          <Image
+            width={240}
+            height={240}
+            src={boy}
+            alt="소개하는 남자 일러스트"
+          />
         </section>
       </article>
       <article>
-        <h3>팔팔 프로젝트에 함께한 사람들</h3>
+        <h3 className="px-10 py-3 bg-subColor2 rounded-full">
+          팔팔 프로젝트에 함께한 사람들
+        </h3>
       </article>
-    </div>
+    </main>
   );
 };
 
