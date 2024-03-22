dd09704450220470ac57bd0e2c922128dc40094e
HaeRheeNam <4ran0318@gmail.com>
2024-03-22 22:38:29 +0900

    [fix] 좋아요 기능 수정 중

--- src/app/(auth)/mypage/page.tsx
+++ src/app/(auth)/mypage/page.tsx
@@ -1,25 +1,25 @@
-'use client';
-import Calendar from '@/components/Calendar';
-import ProfileContents from '@/components/ProfileContents';
-import ProfileImage from '@/components/ProfileImage';
-import ProfileReview from '@/components/ProfileReview';
-import { queryKey } from '@/query/queryKey';
-import { readUsersInfo } from '@/supabase/myPage/profileImage';
-import { useQuery } from '@tanstack/react-query';
-import { useState } from 'react';
+"use client";
+import Calendar from "@/components/Calendar";
+import ProfileContents from "@/components/ProfileContents";
+import ProfileImage from "@/components/ProfileImage";
+import ProfileReview from "@/components/ProfileReview";
+import { queryKey } from "@/query/queryKey";
+import { readUsersInfo } from "@/supabase/myPage/profileImage";
+import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
 
 const MyPage = () => {
-  const myAccount = { email: '1234@qwer.com' };
+  const myAccount = { email: "1234@qwer.com" };
   const userEmail = myAccount.email;
   const [isEdit, setIsEdit] = useState(false);
-  const {
-    isPending,
-    isError,
-    data: userInfo,
-  } = useQuery({
-    queryKey: [queryKey.usersAccounts],
-    queryFn: readUsersInfo,
-  });
+  // const {
+  //   isPending,
+  //   isError,
+  //   data: userInfo,
+  // } = useQuery({
+  //   queryKey: [queryKey.usersAccounts],
+  //   queryFn: readUsersInfo,
+  // });
 
   return (
     <section>
