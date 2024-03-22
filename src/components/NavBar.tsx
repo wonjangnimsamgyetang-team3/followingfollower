6307b06420eb51b2b0cefb85bc2cc28e92ceba95
jihyun-N <gus2726@naver.com>
2024-03-22 20:09:26 +0900

    [feat/#37] - 메인페이지 전체 글 생성 및 기능 구현

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,38 +1,42 @@
-import { User } from '@supabase/auth-helpers-nextjs';
-import Link from 'next/link';
+import { User } from "@supabase/auth-helpers-nextjs";
+import Link from "next/link";
+import Image from "next/image";
+import FFlogo from "@/assets/FFlogo.png";
 
 const NavBar = ({ user }: { user: User | null }) => {
   return (
-    <nav className="flex flex-col place-items-center">
-      <div className="flex">
+    <nav className="flex flex-col place-items-center text-xl">
+      <div className="flex items-center justify-between w-full">
         <Link href="/" className="p-4">
-          FF
+          <Image src={FFlogo} alt="" width={60} height={40} />
         </Link>
-        <Link href="/about" className="p-4">
-          서비스 소개
-        </Link>
-        <Link href="/feed" className="p-4">
-          88피드
-        </Link>
-        {!user ? (
-          <Link href="/login" className="p-4">
-            마이페이지
+        <div>
+          <Link href="/about" className="p-4">
+            서비스 소개
           </Link>
-        ) : (
-          <Link href="/mypage" className="p-4">
-            마이페이지
+          <Link href="/feed" className="p-4">
+            88피드
           </Link>
-        )}
+          {!user ? (
+            <Link href="/login" className="p-4">
+              마이페이지
+            </Link>
+          ) : (
+            <Link href="/mypage" className="p-4">
+              마이페이지
+            </Link>
+          )}
 
-        {user ? (
-          <Link href="/login" className="p-4">
-            로그아웃
-          </Link>
-        ) : (
-          <Link href="/login" className="p-4">
-            로그인
-          </Link>
-        )}
+          {user ? (
+            <Link href="/login" className="p-4">
+              로그아웃
+            </Link>
+          ) : (
+            <Link href="/login" className="p-4">
+              로그인
+            </Link>
+          )}
+        </div>
       </div>
       <div className="w-full h-2 bg-gradient-to-b from-subColor2"></div>
     </nav>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,31 +1,34 @@
-import type { Config } from 'tailwindcss';
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       colors: {
-        mainColor1: '#ff0000',
-        mainColor2: '#890105',
-        subColor1: '#fb8494',
-        subColor2: '#ffd7db',
-        subColor3: '#777777',
-        subColor4: '#efefef',
+        mainColor1: "#ff0000",
+        mainColor2: "#890105",
+        subColor1: "#fb8494",
+        subColor2: "#ffd7db",
+        subColor3: "#777777",
+        subColor4: "#efefef",
       },
       dropShadow: {
-        DEFAULT: '2px 2px 0 #890105',
+        DEFAULT: "2px 2px 0 #890105",
+      },
+      borderRadius: {
+        lg: "50px",
       },
     },
   },
-  plugins: [require('daisyui')],
+  plugins: [require("daisyui")],
 };
 export default config;
