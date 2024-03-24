d9c8bb2e3c802235987836d8469dea8124556283
HaeRheeNam <4ran0318@gmail.com>
2024-03-24 16:54:11 +0900

    [chore] 콘솔 지우기

--- src/components/ProfileImage.tsx
+++ src/components/ProfileImage.tsx
@@ -9,7 +9,6 @@ import { useRouter } from "next/navigation";
 import type { Edit, UserData } from "@/types/type";
 import LogOut from "./LogOut";
 import Image from "next/image";
-import normalImg from "@/assets/profile.png";
 
 const ProfileImage = ({ isEdit, setIsEdit }: Edit) => {
   const router = useRouter();
@@ -77,8 +76,7 @@ const ProfileImage = ({ isEdit, setIsEdit }: Edit) => {
       }
     }
   };
-  console.log(defaultImg);
-  console.log(avatar);
+
   return (
     <div className="bg-subColor4">
       {isEdit ? (
